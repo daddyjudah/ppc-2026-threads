0e09a724f4794dfe748f6078b9d1d5b01b6f8d6e
daddyjudah <moderator647@gmail.com>
2026-03-28 21:13:02 +0300

    fix tbb version2

--- tasks/marin_l_mark_components/tbb/src/ops_tbb.cpp
+++ tasks/marin_l_mark_components/tbb/src/ops_tbb.cpp
@@ -8,7 +8,7 @@
 namespace marin_l_mark_components {
 
 namespace {
-int FindRoot(int *parent, int x) {
+inline int FindRoot(int *parent, int x) {
   int root = x;
   while (parent[root] != root) {
     root = parent[root];
@@ -16,7 +16,7 @@ int FindRoot(int *parent, int x) {
   return root;
 }
 
-void UnionLabels(int *parent, int a, int b) {
+inline void UnionLabels(int *parent, int a, int b) {
   int root_a = FindRoot(parent, a);
   int root_b = FindRoot(parent, b);
   while (root_a != root_b) {
@@ -69,17 +69,18 @@ void MarinLMarkComponentsTBB::FirstPassTBB() {
   int *p_ptr = parent_.data();
   int *l_ptr = labels_flat_.data();
   const uint8_t *b_ptr = binary_flat_.data();
+  int w = width_;
 
-  tbb::parallel_for(tbb::blocked_range<int>(0, height_, 64), [&](const tbb::blocked_range<int> &range) {
+  tbb::parallel_for(tbb::blocked_range<int>(0, height_, 128), [&](const tbb::blocked_range<int> &range) {
     for (int r = range.begin(); r < range.end(); ++r) {
-      int row_off = r * width_;
-      for (int c = 0; c < width_; ++c) {
+      int row_off = r * w;
+      for (int c = 0; c < w; ++c) {
         int idx = row_off + c;
         if (b_ptr[idx] == 0) {
           continue;
         }
 
-        int top = (r > range.begin() && b_ptr[idx - width_]) ? l_ptr[idx - width_] : 0;
+        int top = (r > range.begin() && b_ptr[idx - w]) ? l_ptr[idx - w] : 0;
         int left = (c > 0 && b_ptr[idx - 1]) ? l_ptr[idx - 1] : 0;
 
         if (top == 0 && left == 0) {
@@ -103,11 +104,12 @@ void MarinLMarkComponentsTBB::MergeBordersTBB() {
   int *p_ptr = parent_.data();
   int *l_ptr = labels_flat_.data();
   const uint8_t *b_ptr = binary_flat_.data();
+  int w = width_;
 
   tbb::parallel_for(1, height_, [&](int r) {
-    int curr_row = r * width_;
-    int prev_row = (r - 1) * width_;
-    for (int c = 0; c < width_; ++c) {
+    int curr_row = r * w;
+    int prev_row = curr_row - w;
+    for (int c = 0; c < w; ++c) {
       if (b_ptr[curr_row + c] && b_ptr[prev_row + c]) {
         UnionLabels(p_ptr, l_ptr[curr_row + c], l_ptr[prev_row + c]);
       }
@@ -118,13 +120,12 @@ void MarinLMarkComponentsTBB::MergeBordersTBB() {
 void MarinLMarkComponentsTBB::SecondPassTBB() {
   int *p_ptr = parent_.data();
   int *l_ptr = labels_flat_.data();
+  int total = height_ * width_;
 
-  tbb::parallel_for(0, height_, [&](int r) {
-    int off = r * width_;
-    for (int c = 0; c < width_; ++c) {
-      int idx = off + c;
-      if (l_ptr[idx] != 0) {
-        l_ptr[idx] = FindRoot(p_ptr, l_ptr[idx]);
+  tbb::parallel_for(tbb::blocked_range<int>(0, total, 10000), [&](const tbb::blocked_range<int> &range) {
+    for (int i = range.begin(); i < range.end(); ++i) {
+      if (l_ptr[i] != 0) {
+        l_ptr[i] = FindRoot(p_ptr, l_ptr[i]);
       }
     }
   });
